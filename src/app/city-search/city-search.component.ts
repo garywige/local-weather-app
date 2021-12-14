5b35106d2d5885436e9db7aca863de8945c700e0
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-14 07:40:56 -0800

    City Search Filter/Tap
    Replaced subscription code with rxjs filter & tap

--- src/app/city-search/city-search.component.ts
+++ src/app/city-search/city-search.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { FormControl, Validators } from '@angular/forms'
-import { debounceTime } from 'rxjs'
+import { debounceTime, filter, tap } from 'rxjs'
 
 import { WeatherService } from '../weather/weather.service'
 
@@ -17,15 +17,21 @@ export class CitySearchComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.search.valueChanges.pipe(debounceTime(1000)).subscribe((searchValue: string) => {
-      if (!this.search.invalid) {
-        const userInput = searchValue.split(',').map((s) => s.trim())
+    this.search.valueChanges
+      .pipe(
+        debounceTime(1000),
+        filter(() => !this.search.invalid),
+        tap((searchValue: string) => this.doSearch(searchValue))
+      )
+      .subscribe()
+  }
 
-        this.weatherService.updateCurrentWeather(
-          userInput[0],
-          userInput.length > 1 ? userInput[1] : undefined
-        )
-      }
-    })
+  doSearch(searchValue: string) {
+    const userInput = searchValue.split(',').map((s) => s.trim())
+
+    this.weatherService.updateCurrentWeather(
+      userInput[0],
+      userInput.length > 1 ? userInput[1] : undefined
+    )
   }
 }
