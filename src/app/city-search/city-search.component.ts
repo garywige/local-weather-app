7f3eb40fdba88bb15adbeef52a3be310378e7beb
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-13 19:31:33 -0800

    Added Input Validation

--- src/app/city-search/city-search.component.ts
+++ src/app/city-search/city-search.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core'
-import { FormControl } from '@angular/forms'
+import { FormControl, Validators } from '@angular/forms'
 import { debounceTime } from 'rxjs'
 
 import { WeatherService } from '../weather/weather.service'
@@ -13,12 +13,12 @@ export class CitySearchComponent implements OnInit {
   search: FormControl
 
   constructor(private weatherService: WeatherService) {
-    this.search = new FormControl()
+    this.search = new FormControl('', [Validators.minLength(2)])
   }
 
   ngOnInit(): void {
     this.search.valueChanges.pipe(debounceTime(1000)).subscribe((searchValue: string) => {
-      if (searchValue) {
+      if (!this.search.invalid) {
         const userInput = searchValue.split(',').map((s) => s.trim())
 
         this.weatherService
